f3194edd8c444847ad0b2233e51b0681e5777dcb
viraj-10 <virajjoshi132@gmail.com>
2024-07-10 12:16:54 +0530

    fix: dev script

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: "media",
+  darkMode: "class",
   content: ["./app/**/*.{tsx,jsx,ts,js}", "./components/**/*.{tsx,jsx,ts,js}"],
   presets: [require('nativewind/preset')],
   safelist: [
